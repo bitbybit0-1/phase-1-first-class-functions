236c02e36005becf98df8fc03a6c55cd3c8e0a38
Joseph Sutera <joesutera11@gmail.com>
2022-07-08 19:12:03 -0400

    commit tree 2cdedcdcb744fc83e03802a0a4faaa797526926f

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+function receivesAFunction(callback){
+callback();
+
+}
+    function returnsANamedFunction(){
+        return function name(){
+            let x=y;
+        }
+    }
+function returnsAnAnonymousFunction(){
+    return function (){
+        let nothing = "something";
+    }
+}
